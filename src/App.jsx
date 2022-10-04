0ba3d61cb7e32362185ee8f76a0a212fe6487ef2
Dojeto <omlimdiwala25@gmail.com>
2022-10-05 00:45:36 +0530

    ServerSide Render Issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes , Route } from "react-router-dom";
-import Hello from "./Components/Home";
 import Admin from "./Components/Admin/Admin";
 import "./App.scss";
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path="*" element = {<Hello/>}/>
       <Route path="/admin" element = {<Admin/>}/>
     </Routes>
     </>
