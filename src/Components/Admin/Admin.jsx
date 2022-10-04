49fe107523fab47154587b464b5de068eece784f
Dojeto <omlimdiwala25@gmail.com>
2022-10-05 01:47:17 +0530

    Sidebar updated

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import Sidebar from "../Slidebar/Sidebar";
+import Sidebar from "../Sidebar/Sidebar";
 import test from "./test.module.scss"
-
+import { FaList, FaRegHeart } from "react-icons/fa";
+import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import {IoIosArrowForward,IoIosArrowBack} from "react-icons/io"
+import { RiPencilLine } from "react-icons/ri";
+import { BiCog } from "react-icons/bi";
+//logos={[<FaList />,<FaRegHeart />,<RiPencilLine />,<BiCog />]}/>
+//"users","dnsjad","dnsja","dnsahbd"
 const Admin = ()=>{
     return(
     <>
     <div className={test.container}>
-        <Sidebar/>
+        <Sidebar 
+        catagories={[
+            {detail:"user",logos:<FaList />},
+            {detail:"analitics",logos:<FaRegHeart />},
+            {detail:"test",logos:<RiPencilLine />},
+            {detail:"test2",logos:<BiCog />}]}
+        />
         <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, ut sapiente. Alias sed molestiae suscipit ipsam a sunt adipisci explicabo quam nisi tenetur! Sint repellendus ea sequi at consequatur deleniti?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, nam. Voluptatibus animi quaerat delectus, temporibus nesciunt sequi, ratione provident, quasi dolorem veniam modi officiis natus nobis similique sit labore voluptatem. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa doloribus esse id voluptatem temporibus voluptatibus odio quia, quo harum doloremque tenetur et hic consequuntur, deserunt quibusdam repellendus iste consequatur recusandae.</h1>
     </div>
     </>
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -10,18 +10,15 @@ import {
   SidebarHeader,
   SidebarFooter,
   SidebarContent,
-} from "react-pro-sidebar";
+} from "react-pro-sidebar"; 
 
-import { FaList, FaRegHeart } from "react-icons/fa";
-import { FiHome, FiLogOut, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { FiHome, FiLogOut} from "react-icons/fi";
 import {IoIosArrowForward,IoIosArrowBack} from "react-icons/io"
-import { RiPencilLine } from "react-icons/ri";
-import { BiCog } from "react-icons/bi";
 import "react-pro-sidebar/dist/css/styles.css";
 import "./Header.scss";
 
 
-const Sidebar = () => {
+const Sidebar = (props) => {
   
     const [menuCollapse, setMenuCollapse] = useState(false)
 
@@ -60,10 +57,9 @@ const Sidebar = () => {
               <MenuItem active={true} icon={<FiHome />}>
                 Home
               </MenuItem>
-              <MenuItem icon={<FaList />}>Category</MenuItem>
-              <MenuItem icon={<FaRegHeart />}>Favourite</MenuItem>
-              <MenuItem icon={<RiPencilLine />}>Author</MenuItem>
-              <MenuItem icon={<BiCog />}>Settings</MenuItem>
+              {props.catagories.map((e)=>{
+                return(<MenuItem icon={e.logos}>{e.detail}</MenuItem>)
+              })}
             </Menu>
           </SidebarContent>
           <SidebarFooter>
