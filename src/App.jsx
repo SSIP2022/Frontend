d2aea8e892727a9182e216071e1b289f4d54b114
Mann Patel <mannpatel3118@gmail.com>
2022-10-05 16:41:59 +0530

    added user complaint  page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Admin from "./pages/admin/index";
 import "./App.scss";
 import Root from "./pages/root";
-import User from "./pages/user";
+import User from "./pages/user/index";
 import Department from "./pages/department";
 import AdminAnalytics from "./pages/admin/components/AdminAnalytics";
 import UserAnalytics from "./pages/user/components/UserAnalytics";
@@ -13,9 +13,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Root />} />
-        <Route path="/user" element={<User />}>
-          <Route path="analytics" element={<UserAnalytics />} />
-        </Route>
+        <Route path="/user" element={<User/>}/>
         <Route path="/department" element={<Department />} />
         <Route path="/admin" element={<Admin />}/>
       </Routes>
